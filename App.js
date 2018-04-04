f0e2b638e7c3a92a48716276b68d43764eca0391
Mona Lisa <37361658+SaraAgreed@users.noreply.github.com>
2018-04-04 14:33:04 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var date = new Date();
+var hours = date.getHours();
+var mins = date.getMinutes();
+var sec = date.getSeconds();
+var day = date.getDate();
+var month = date.getMonth();
+var year = date.getFullYear();
+
+app.get('/messenger', function(req,res){
+  var messenger =  {
+    "user1" : 
+    [
+      {
+      "name" : "Sara",
+      "msg" : "Hello Sir",
+      "time" : [hours, ":", mins, ":", sec],
+      "date" : [day,month, ",", year]     
+    }
+  ],
+    "user2": 
+    [
+      {
+      "name" : "Hamza Sir",
+      "msg" : "Hello Sara",
+      "date and time" : Date(),
+    }
+  ]
+  }
+  return res.json(messenger)
+});
+app.listen(3000);
